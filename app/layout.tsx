4ee6f5319bd15f045bec927e30bf5bfe9cbf1f6b
Alvin Christian <alvin.christian7@yahoo.com>
2024-06-09 17:36:13 +0700

    configure PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,60 @@
 import '@mantine/core/styles.css';
 import { Suspense } from 'react';
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
+import type { Metadata, Viewport } from 'next';
+import type { ReactNode } from 'react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { theme } from '../theme';
 import WithSearchBarLayout from './WithSearchBarLayout';
 import Providers from './providers';
 
-export const runtime = 'edge';
+const APP_NAME = 'Ultimate Anime Collection App';
+const APP_DEFAULT_TITLE = 'Ultimate Anime Collection';
+const APP_TITLE_TEMPLATE = '%s - PWA App';
+const APP_DESCRIPTION = 'Full of animes, and some of them are yours!';
 
-export const metadata = {
-  title: 'Ultimate Anime Collection',
-  description: 'Full of animes, and some of them are yours!',
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: 'website',
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: 'summary',
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
-export default function RootLayout({ children }: { children: any }) {
+export const viewport: Viewport = {
+  themeColor: '#FFFFFF',
+};
+
+export const runtime = 'edge';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@ import bundleAnalyzer from '@next/bundle-analyzer';
 import withSerwistInit from '@serwist/next';
 
 const withSerwist = withSerwistInit({
-  swSrc: 'sw.ts', // where the service worker src is
+  swSrc: 'app/sw.ts', // where the service worker src is
   swDest: 'public/sw.js', // where the service worker code will end up
-  // disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development",
 });
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
